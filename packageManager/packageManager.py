93dfd2ef6925ba9d3b204f0809930a3d57fe5fe0
George <george.dickinson@gmail.com>
2020-10-24 21:15:22 -0600

    packageManager plugin update
    added pip and conda install function

--- packageManager/packageManager.py
+++ packageManager/packageManager.py
@@ -1,5 +1,5 @@
-from __future__ import (absolute_import, division,print_function, unicode_literals)
-from future.builtins import (bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
+#from __future__ import (absolute_import, division,print_function, unicode_literals)
+#from future.builtins import (bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
 from qtpy import QtWidgets, QtCore, QtGui
 import flika
 flika_version = flika.__version__
@@ -13,6 +13,12 @@ import time
 from distutils.version import StrictVersion
 import sys
 import subprocess
+import json
+import os
+
+#import conda.cli.python_api as Conda
+
+from .defaultPackageList import defaultCondaPackageList
 
 flika_version = flika.__version__
 if StrictVersion(flika_version) < StrictVersion('0.2.23'):
@@ -34,6 +40,7 @@ class PackageManager(BaseProcess_noPriorWindow):
         BaseProcess_noPriorWindow.__init__(self)
         self.packageList = []
         self.listDisplay = ListWindow()
+        self.condaENV = (os.environ['CONDA_DEFAULT_ENV'])
         return
 
     def __call__(self):
@@ -43,15 +50,19 @@ class PackageManager(BaseProcess_noPriorWindow):
     def gui(self):
         self.gui_reset()
         
-        self.getPackages = QtWidgets.QPushButton('Get Packages')
-        self.getPackages.pressed.connect(self.showPackages)        
+        self.showPackages_button = QtWidgets.QPushButton('Show Packages')
+        self.showPackages_button.pressed.connect(self.showPackages)   
         
-        self.items.append({'name':'getPackages','string':'Show currently installed python packages: ', 'object': self.getPackages})  
+        self.installPackages_button = QtWidgets.QPushButton('Install Packages')
+        self.installPackages_button.pressed.connect(self.installPackages)         
+        
+        self.items.append({'name':'getPackages','string':'Show currently installed python packages: ', 'object': self.showPackages_button})  
+        self.items.append({'name':'installPackages','string':'Install default python packages: ', 'object': self.installPackages_button})  
+        
         super().gui()
         return
 
     def showPackages(self):
-        import json
         proc = subprocess.run(["conda", "list", "--json"], text=True, capture_output=True)
         self.packageList = json.loads(proc.stdout)
         self.listDisplay.clear()
@@ -61,10 +72,62 @@ class PackageManager(BaseProcess_noPriorWindow):
         return
 
 
-    def conda_install(self, environment, *package):
-        proc = run(["conda", "install", "--quiet"] + packages,
-                   text=True, capture_output=True)
-        return json.loads(proc.stdout)
+    def installPackages(self, default=True, level='>='):
+        if default:
+            installPackages = []
+            for package in defaultCondaPackageList:
+                item = str(package['name'] + level + package['version'])
+                installPackages.append(item)
+            #print(installPackages)
+        packageStr = " ".join(installPackages)
+        
+        self.install(installPackages, level)
+
+        
+
+    def install(self, packages,level):
+        condaInstall = []
+        pipInstall = []
+        uninstalled = []
+        for package in packages:
+            print('-------------------')            
+            print(package)
+            print('-------------------')
+            try:
+                try:
+                    try:
+                        subprocess.check_call([sys.executable, '-m', 'conda', 'install', 
+                                       package, '-y'])
+                        condaInstall.append(package)
+               
+                    except:
+                        subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                                               package, '--no-cache-dir'])
+                        pipInstall.append(package)
+            
+                except:
+                    package = package.split(level)[0]
+                    try:
+                        subprocess.check_call([sys.executable, '-m', 'conda', 'install', 
+                                       package, '-y'])
+                        condaInstall.append(package)
+               
+                    except:
+                        subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                                               package, '--no-cache-dir'])
+                        pipInstall.append(package)                
+            except:
+               uninstalled.append(package) 
+        
+        print('Conda examined: ', condaInstall)
+        print('-------------------')
+        print('Pip examined: ', pipInstall)        
+        print('-------------------') 
+        print('Uninstalled: ', uninstalled)          
+        print('Restart FLIKA to load new libraries')
+        print('-------------------')
+        return        
+
 
     def closeEvent(self, event):
         BaseProcess_noPriorWindow.closeEvent(self, event)
@@ -93,5 +156,7 @@ class ListWindow(QtWidgets.QWidget):
         
     def clear(self):
         self.listwidget.clear()
+
+
 
 packageManager = PackageManager()
