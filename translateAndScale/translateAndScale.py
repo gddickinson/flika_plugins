6a4683996b540f87f0cbdb49171a0c11f414f84d
George Dickinson <george.dickinson@gmail.com>
2024-01-28 11:16:23 -0500

    translateAndScale update
    FIxed ROI rotation problem by moving handle instead of using ROI.rotate()

--- translateAndScale/translateAndScale.py
+++ translateAndScale/translateAndScale.py
@@ -33,12 +33,13 @@ import os, shutil, subprocess
 import math
 import sys
 
-from scipy.ndimage import center_of_mass, gaussian_filter, binary_fill_holes, binary_closing, label, find_objects
+from scipy.ndimage import center_of_mass, gaussian_filter, binary_fill_holes, binary_closing, label, find_objects, zoom
 from skimage.filters import threshold_otsu
-
+from scipy.ndimage import rotate as nd_rotate
 from skimage.registration import phase_cross_correlation
 from skimage.transform import warp_polar, rotate, rescale
-from skimage.util import img_as_float
+
+import pandas as pd
 
 #from pyqtgraph.canvas import Canvas, CanvasItem
 
@@ -61,7 +62,129 @@ else:
 from pyqtgraph.dockarea.Dock import Dock
 from pyqtgraph.dockarea.DockArea import DockArea
 
+from .io import *
 
+crossbowShape = np.array([
+ [0,0,0,1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,1,1,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,1,1,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0],
+ [0,0,0,1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0,0,0,0]])
+
+hShape = np.array([
+ [0,0,0,1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,0,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,1,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,0,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [0,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,0,0],
+ [0,0,1,1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,1,0,0],
+ [0,0,0,1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,0,0,0]])
+
+yShape = np.array([
+[1,1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,1],
+[1,1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,1],
+[1,1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1],
+[0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,0],
+[0,0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0],
+[0,0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,0],
+[0,0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,0],
+[0,0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,0],
+[0,0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
+[0,0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
+])
+
+def rotate_around_point(x,y, angle, origin=(0,0)):
+    radians = angle * math.pi / 180
+    offset_x, offset_y = origin
+    adjusted_x = (x - offset_x)
+    adjusted_y = (y - offset_y)
+    cos_rad = math.cos(radians)
+    sin_rad = math.sin(radians)
+    dx = offset_x + cos_rad * adjusted_x + sin_rad * adjusted_y
+    dy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
+    return dx, dy
+
+
+
+class FileSelector(QWidget):
+    """
+    This widget is a button with a label.  Once you click the button, the widget waits for you to select a file to save.  Once you do, it sets self.filename and it sets the label.
+    """
+    valueChanged=Signal()
+    def __init__(self,filetypes='*.*', mainGUI=None):
+        QWidget.__init__(self)
+
+        self.mainGUI = mainGUI
+
+        self.button=QPushButton('Load Data')
+        self.label=QLabel('None')
+        self.window=None
+        self.layout=QHBoxLayout()
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+        self.button.clicked.connect(self.buttonclicked)
+        self.filetypes = filetypes
+        self.filename = ''
+        self.columns = []
+
+
+    def buttonclicked(self):
+        if g.win == None:
+            g.alert('Load tiff stack and set as current window first')
+            return
+        prompt = 'testing fileSelector'
+        self.filename = open_file_gui(prompt, filetypes=self.filetypes)
+        self.label.setText('...'+os.path.split(self.filename)[-1][-20:])
+        self.valueChanged.emit()
+
+    def value(self):
+        return self.filename
+
+    def setValue(self, filename):
+        self.filename = str(filename)
+        self.label.setText('...' + os.path.split(self.filename)[-1][-20:])
 
 class DisplayParams():
     '''
@@ -213,11 +336,37 @@ class TemplateROI(pg.RectROI):
 
         self.updateDisplay()
 
+    def getPattern(self):
+        return self.template
+
     def getCenter(self):
         posX,posY = self.pos()
         sizeX,sizeY = self.size()
         return (int(posX+sizeX/2), int(posY+sizeY/2))
 
+
+    def _rotate(self, angle):
+        newAngle = self.angle()+angle
+        newState = self.stateCopy()
+        #origin = self.transformOriginPoint()
+
+        br = self.boundingRect()
+        center = br.center()
+
+        self.setTransformOriginPoint(center)
+        self.setRotation(newAngle)
+        self.setPos(newState['pos'], update=True)
+        self.setAngle(newAngle, update=True)
+
+
+        self.stateChanged(finish=True)
+
+    def stateRect(self, state):
+        r = QtCore.QRectF(0, 0, state['size'][0], state['size'][1])
+        tr = QtGui.QTransform()
+        tr.rotate(state['angle'])
+        r = tr.mapRect(r)
+        return r.adjusted(state['pos'][0], state['pos'][1], state['pos'][0], state['pos'][1])
 
     def getAngle(self):
         return self.angle() % 360
@@ -237,14 +386,9 @@ class TemplateROI(pg.RectROI):
             self.display.angle_text.setText('{}'.format(round(angle, 2)))
             self.display.size_text.setText('{}'.format(round(size, 2)))
 
-shapeSize = [400,400]
-startPosition = [80, 50]
 
-disk_template = TemplateROI(pos=startPosition, size=shapeSize, template = 'disk', pen=(0,9))
-square_template = TemplateROI(pos=startPosition, size=shapeSize, template = 'square', pen=(0,9))
-crossbow_template = TemplateROI(pos=startPosition, size=shapeSize, template = 'crossbow', pen=(0,9))
-yShape_template = TemplateROI(pos=startPosition, size=shapeSize, template = 'y-shape', pen=(0,9))
-hShape_template = TemplateROI(pos=startPosition, size=shapeSize, template = 'h-shape', pen=(0,9))
+
+
 
 '''
 #####################################################################################################################################
@@ -309,23 +453,40 @@ class TranslateAndScale(BaseProcess_noPriorWindow):
         self.startButton = QPushButton('Start Alignment')
         self.startButton.pressed.connect(self.startAlign)
 
-        self.endButton = QPushButton('Finished Alignment')
+        self.endButton = QPushButton('Save Alignment')
         self.endButton.pressed.connect(self.endAlign)
 
+        self.clearButton = QPushButton('Clear Alignment')
+        self.clearButton.pressed.connect(self.clearTemplate)
 
         self.templateBox = pg.ComboBox()
-        self.templates = {'Disk': disk_template,
-                          'Square': square_template,
-                          'Crossbow': crossbow_template,
-                          'Y-shape': yShape_template,
-                          'H-shape': hShape_template
+        self.templates = {'Disk': 'disk',
+                          'Square': 'square',
+                          'Crossbow': 'crossbow',
+                          'Y-shape': 'y-shape',
+                          'H-shape': 'h-shape'
                           }
         self.templateBox.setItems(self.templates)
+        self.templateBox.activated.connect(self.update)
 
-        self.items.append({'name': 'dataWindow', 'string': 'Data Window', 'object': self.dataWindow})
+        #data file selector
+        self.getFile = FileSelector(filetypes='*.csv', mainGUI=self)
+
+        #connections
+        self.getFile.valueChanged.connect(self.loadData)
+
+        self.transformDataButton = QPushButton('Transform data')
+        self.transformDataButton.pressed.connect(self.transformData)
+
+
+        self.items.append({'name': 'dataWindow', 'string': 'Image Window', 'object': self.dataWindow})
+        self.items.append({'name': 'filename ', 'string': 'Data File', 'object': self.getFile})
         self.items.append({'name': 'template', 'string': 'Choose template', 'object': self.templateBox})
         self.items.append({'name': 'startButton', 'string': '', 'object': self.startButton})
         self.items.append({'name': 'endButton', 'string': '', 'object': self.endButton})
+        self.items.append({'name': 'clearButton', 'string': '', 'object': self.clearButton})
+        self.items.append({'name': 'clearButton', 'string': '', 'object': self.clearButton})
+        self.items.append({'name': 'transformButton', 'string': 'Transform data file', 'object': self.transformDataButton})
 
         super().gui()
 
@@ -333,25 +494,38 @@ class TranslateAndScale(BaseProcess_noPriorWindow):
         self.displayParams = DisplayParams(self)
         self.displayParams.show()
 
+        self.df = None
+
+        self.filename = ''
+        self.dataDF = None
+        self.plotWindow = None
+
+        self.pointMapScatter = None
+
         return
 
     def startAlign(self):
+        shapeSize = [400,400]
+        startPosition = [80, 50]
         #initiate template
-        template = self.getValue('template')
-        template.addDisplay(self.displayParams)
-        self.getValue('dataWindow').imageview.addItem(template)
-        self.currentTemplate = template
+        if self.currentTemplate == None:
+            template = TemplateROI(pos=startPosition, size=shapeSize, template = self.getValue('template'), pen=(0,9))
+            self.currentTemplate = template
+
+        self.currentTemplate.addDisplay(self.displayParams)
+        self.getValue('dataWindow').imageview.addItem(self.currentTemplate)
+
 
         #autodetect micropattern
         img = self.getValue('dataWindow').image
 
-        print(img.shape)
+        #print(img.shape)
 
         #get max projection if stack
         if len(img.shape) > 2:
             img = np.max(img,0)
 
-        print(img.shape)
+        #print(img.shape)
 
         #blur
         img = gaussian_filter(img, 3)
@@ -377,17 +551,17 @@ class TranslateAndScale(BaseProcess_noPriorWindow):
 
         # Get the height and width
         height = int(objs[0][0].stop - objs[0][0].start)
-        width = int(objs[0][1].stop - objs[0][1].start)
+        #width = int(objs[0][1].stop - objs[0][1].start)
 
         center = center_of_mass(img)
         if len(center) == 2:
             x,y = center[0], center[1]
         else:
             x,y = center[1], center[2]
-        print(x,y)
+        #print(x,y)
 
         #display autodetected micropattern binary
-        Window((img))
+        #Window((img))
 
         #attempt to scale
         self.currentTemplate.scale(height/self.currentTemplate.size()[0])
@@ -403,6 +577,16 @@ class TranslateAndScale(BaseProcess_noPriorWindow):
 
         if self.currentTemplate.template == 'square':
             rotShape = np.ones((shapeSize ,shapeSize ))
+
+        elif self.currentTemplate.template == 'h-shape':
+            rotShape = zoom(hShape, shapeSize/hShape.shape[0])
+
+        elif self.currentTemplate.template == 'y-shape':
+            rotShape = zoom(yShape, shapeSize/yShape.shape[0])
+
+        elif self.currentTemplate.template == 'crossbow':
+            rotShape = zoom(crossbowShape, shapeSize/yShape.shape[0])
+
         else:
             return
 
@@ -422,15 +606,143 @@ class TranslateAndScale(BaseProcess_noPriorWindow):
         print('rotation: {}'.format(shifts[0]))
 
         #attempt to rotate to match micropattern template
-        self.currentTemplate.rotate(angleToRotate)
+        #roiSize = self.currentTemplate.getSize()
+        #self.currentTemplate._rotate(angleToRotate) #THIS ROTATION AROUND CORNER POS - CAN'T FIGURE OUT HOW TO FIX! Try moving roation handle instead
 
-        Window(rotated)
+        #move rotation handle to rotate roi
+        handles = self.currentTemplate.getHandles()
+        print(handles)
+        handlesPos = self.currentTemplate.getLocalHandlePositions()
+        print(handlesPos)
+        rotX,rotY = (handlesPos[1][1][0],handlesPos[1][1][1])
+        print(rotX,rotY)
+        newX, newY = rotate_around_point(rotX,rotY, angleToRotate, origin=(0,0))
+        print(newX,newY)
+
+        self.currentTemplate.movePoint(handles[1],QPointF(newX,newY))
+
+
+        #Rotation still not working - probably need to align shape center with roi...
+        # newAngle = self.currentTemplate.getAngle() + angleToRotate
+
+        # roiSize = self.currentTemplate.getSize()
+        # print(roiSize)
+        # self.currentTemplate.setAngle(newAngle, centerLocal=(roiSize,roiSize))
+
+
+        #Window(rotated)
 
         return
 
     def endAlign(self):
+        #export rotation file
+        fileName = self.getValue('dataWindow').filename
+
+        d = {'center_x' : self.currentTemplate.getCenter()[0],
+        'center_y': self.currentTemplate.getCenter()[1],
+        'angle' : self.currentTemplate.getAngle(),
+        'size' : self.currentTemplate.getSize(),
+        'pattern' : self.currentTemplate.getPattern(),
+        'file' : fileName}
+
+        self.df = pd.DataFrame(data=d, index=[0])
+
+
+        saveName = os.path.splitext(fileName)[0] + '_align.txt'
+        self.df.to_csv(saveName)
+        print('\n alignment file exported to {}'.format(saveName))
+
+
+        return
+
+    def clearTemplate(self):
+        #clear roi
         self.getValue('dataWindow').imageview.removeItem(self.currentTemplate)
         self.currentTemplate = None
+
+    def update(self):
+        self.clearTemplate()
+        self.startAlign()
+
+    def loadData(self):
+        # Set the plot window to the global window instance
+        img = self.getValue('dataWindow').image
+        if len(img.shape) > 2:
+            img = np.max(img,0)
+
+        self.plotWindow = Window(img)
+
+        # Get the filename from the GUI
+        self.filename = self.getFile.value()
+
+        # Load the data from the selected file using Pandas
+        self.data = pd.read_csv(self.filename)
+        # create new df to store transform points
+        self.transformDF = self.data[['x','y']]
+
+        #plot data on image
+        self.plotDataPoints()
+
+
+    def plotDataPoints(self):
+        if self.pointMapScatter != None:
+            self.plotWindow.imageview.view.removeItem(self.pointMapScatter)
+
+
+        # Create a ScatterPlotItem and add it to the ImageView
+        self.pointMapScatter = pg.ScatterPlotItem(size=2, pen=None, brush=pg.mkBrush(30, 255, 35, 255))
+        self.pointMapScatter.setSize(2, update=False)
+        self.pointMapScatter.setData(self.transformDF['x'], self.transformDF['y'])
+        self.plotWindow.imageview.view.addItem(self.pointMapScatter)
+
+
+
+    def transformData(self):
+        target_X = 0
+        target_Y = 0
+        target_angle = 0
+        target_size = 400
+
+        #get angle to rotate
+        angle = target_angle + self.currentTemplate.getAngle()
+        # Get the center of the dataframe
+        center = self.data[['x', 'y']].mean()
+        #rotate data points around center
+        self.transformDF['x'],self.transformDF['y'] = rotate_around_point(self.data['x'],self.data['y'], angle, origin=center)
+
+        #center and rotate img
+        img = self.getValue('dataWindow').image
+        if len(img.shape) > 2:
+            img = np.max(img,0)
+
+        #pad image with 2000x2000 pixels to provide room for cropping to center of micropattern
+        img = np.pad(img, 2000, mode='constant')
+
+        #crop micropattern based on point data center
+        crop_width = 400
+        img = img[int(center[0]-crop_width)+2000:int(center[0]+crop_width+2000), int(center[1]-crop_width)+2000: int(center[1]+crop_width)+2000]
+
+        #pad edges
+        pad_width = 400
+        img = np.pad(img, pad_width, mode='constant')
+
+        #rotate image
+        img= nd_rotate(img, angle= -angle, reshape=True)
+
+        #crop img
+        #img = img[padX:padX+h,padY:padY+w]
+
+        #center points on image
+        imgCenter = (img.shape[0]/2, img.shape[1]/2)
+        self.transformDF['x'] = self.transformDF['x'] + imgCenter[0] - center[0]
+        self.transformDF['y'] = self.transformDF['y'] + imgCenter[1] - center[1]
+
+        #img = rotateImg(img, -angle, pivot=center)
+        self.plotWindow.imageview.setImage(img)
+
+        #replot
+        self.plotDataPoints()
+
         return
 
 
